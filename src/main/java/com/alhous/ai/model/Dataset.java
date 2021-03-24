0c1c53a5f519297e781f2d1187f6378073abc961
Silahi Alihoussene <silahi.alihoussene@gmail.com>
2021-03-24 11:40:36 +0000

    Dataset class created

---
+++ src/main/java/com/alhous/ai/model/Dataset.java
@@ -0,0 +1,25 @@
+package com.alhous.ai.model;
+
+import java.util.List;
+
+public class Dataset {
+    private String name;
+    private List<Category> categories;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+}
